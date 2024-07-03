238e91e7e38acaa7f0e23c81054dc1c0c3af31fc
its_Rohit <113571542+Rohitwagh1406@users.noreply.github.com>
2024-07-03 19:16:59 +0530

    Create script2.js

---
+++ static/script2.js
@@ -0,0 +1,23 @@
+// var socket = io.connect("wss://stt-api-final-v2.onrender.com:443");
+var socket = io.connect("wss://cdn.socket.io/4.7.5/socket.io.js"); 
+
+  
+  var isTranscribing = false;
+  
+  document.getElementById("startButton").addEventListener("click", function () {
+    if (!isTranscribing) {
+      // Start transcription
+      isTranscribing = true;
+      socket.emit("toggle_transcription", { action: "start" });
+      document.getElementById("startButton").innerText = "Stop";
+    } else {
+      // Stop transcription
+      isTranscribing = false;
+      socket.emit("toggle_transcription", { action: "stop" });
+      document.getElementById("startButton").innerText = "Start";
+    }
+  });
+  
+  socket.on("transcription_update", function (data) {
+    document.getElementById("captions").innerHTML = data.transcription;
+  });
